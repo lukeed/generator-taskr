8bf95394145c921c2cd3d187e6696ce8d19f0b5a
Jorge Bucaran <jbucaran@me.com>
2015-08-20 06:23:02 +0900

    add filter examples in the generated code

--- generators/app/templates/index.es6.js
+++ generators/app/templates/index.es6.js
@@ -1,5 +1,34 @@
 export default function () {
-  this.filter("<%= pluginSlugName %>", (source, options) => {
-    /* return { code, map, ext } */
+  this.filter("<%= pluginSlugName %>", (data, options) => {
+  /**
+    @overview A filter plugin returns an object { code, map, ext }
+    which is the result of transforming the incomding data source:
+
+      return { code, map, ext }
+
+    @example Sync filter `j` that transforms a given string into an
+    object, i.e, {code, map} where code is the result data and map
+    a sourcemap if `options.sourceMap === true`.
+
+      const j = require("my-js-transformer")
+      const assign = require("object-assign")
+
+      export default function () {
+        return this.filter("j", (data, options) => {
+          return assign({ ext: ".js"}, j.render(data.toString(), options))
+        })
+      }
+
+    @example Async filter `s` that transforms a given string and invokes
+    a callback function with an object, i.e, {css, map}.
+
+    const s = require("my-style-trasformer")
+    const assign = require("object-assign")
+
+    export default function () {
+      return this.defer(s.render)(data.toString(), options).then((result) =>
+        assign({ ext: ".css"}, result))
+    }
+   */
   })
 }
