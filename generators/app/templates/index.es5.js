837b98dca4a8dff521986def6d85fb1e2d850a11
Luke Edwards <luke.edwards05@gmail.com>
2016-04-06 08:34:29 -0700

    updates to match ^1.0 development
    * updates to match ^1.0 development
    
    * remove refs to eslintrc file

--- generators/app/templates/index.es5.js
+++ generators/app/templates/index.es5.js
@@ -1,5 +1,5 @@
 module.exports = function (/*debug*/) {
-  this.filter("<%= pluginSlugName %>", (data, options) => {
+  this.filter('<%= pluginSlugName %>', (data, options) => {
   /**
     @overview A filter plugin returns an object { code, map, ext }
     which is the result of transforming the incomding data source:
@@ -10,24 +10,24 @@ module.exports = function (/*debug*/) {
     object, i.e, {code, map} where code is the result data and map
     a sourcemap if `options.sourceMap === true`.
 
-      const j = require("my-js-transformer")
-      const assign = require("object-assign")
+      const j = require('my-js-transformer')
+      const assign = require('object-assign')
 
       module.exports = function () {
-        return this.filter("j", (data, options) => {
-          return assign({ ext: ".js"}, j.render(data.toString(), options))
+        return this.filter('j', (data, options) => {
+          return assign({ ext: '.js'}, j.render(data.toString(), options))
         })
       }
 
     @example Async filter `s` that transforms a given string and invokes
     a callback function with an object, i.e, {css, map}.
 
-    const s = require("my-style-trasformer")
-    const assign = require("object-assign")
+    const s = require('my-style-trasformer')
+    const assign = require('object-assign')
 
     module.exports = function () {
       return this.defer(s.render)(data.toString(), options).then((result) =>
-        assign({ ext: ".css"}, result))
+        assign({ ext: '.css'}, result))
     }
   */
   })
