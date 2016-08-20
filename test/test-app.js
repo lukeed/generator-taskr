6ec0f5fb6e5e60a35010615a356be3fc80f4d418
Oleh Kuchuk <kuchuklehjs@gmail.com>
2016-08-20 15:15:51 +0300

    added linting

--- test/test-app.js
+++ test/test-app.js
@@ -1,29 +1,28 @@
-"use strict"
+'use strict'
 
-var path = require("path")
-var assert = require("yeoman-generator").assert
-var helpers = require("yeoman-generator").test
-var os = require("os")
+var path = require('path')
+var assert = require('yeoman-generator').assert
+var helpers = require('yeoman-generator').test
 
-describe("generator fly plugin:app", function () {
+describe('generator fly plugin:app', function () {
   before(function (done) {
-    helpers.run(path.join(__dirname, "../generators/app"))
-      .withOptions({ skipInstall: true })
-      .withPrompts({ someOption: true })
-      .on("end", done)
+    helpers.run(path.join(__dirname, '../generators/app'))
+      .withOptions({skipInstall: true})
+      .withPrompts({someOption: true})
+      .on('end', done)
   })
 
-  it("creates files", function () {
+  it('creates files', function () {
     assert.file([
-      "CHANGELOG.md",
-      "index.js",
-      ".travis.yml",
-      ".editorconfig",
-      "README.md",
-      "package.json",
-      "LICENSE",
-      ".gitignore",
-      "test/test.js"
+      'CHANGELOG.md',
+      'index.js',
+      '.travis.yml',
+      '.editorconfig',
+      'README.md',
+      'package.json',
+      'LICENSE',
+      '.gitignore',
+      'test/test.js'
     ])
   })
 })
