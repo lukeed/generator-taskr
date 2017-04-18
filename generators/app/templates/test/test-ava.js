6ef75c1bcf748c61b95a7140a68a20baf7e2c897
Luke Edwards <luke.edwards05@gmail.com>
2017-04-17 17:12:57 -0700

    set default tape & ava tests

--- generators/app/templates/test/test-ava.js
+++ generators/app/templates/test/test-ava.js
@@ -1,5 +1,22 @@
-const test = require("ava")
+const join = require('path').join
+const test = require('ava')
+const Fly = require('fly')
 
-test("<%= pluginName %> works properly", (t) => {
-  t.pass()
+const dir = join(__dirname, 'fixtures')
+const plugins = [require('fly-clear'), require('../')]
+
+const tmpDir = str => join(__dirname, str)
+const create = tasks => new Fly({tasks, plugins})
+
+test('attach `<%= pluginSlugName %>` plugin to instance', t => {
+  const fly = create();
+  t.ok('<%= pluginSlugName %>' in fly.plugins);
+})
+
+test('attach `<%= pluginSlugName %>` to Task instance', t => {
+  create({
+    *foo(f) {
+      t.ok('<%= pluginSlugName %>' in f);
+    }
+  }).start('foo')
 })
--- generators/app/templates/test/test-tape.js
+++ generators/app/templates/test/test-tape.js
@@ -1,6 +1,20 @@
-const test = require("tape").test
+const join = require('path').join
+const test = require('tape')
+const Fly = require('fly')
 
-test("<%= pluginName %>", (t) => {
-  t.pass()
-  t.end()
+const dir = join(__dirname, 'fixtures')
+const plugins = [require('fly-clear'), require('../')]
+
+const tmpDir = str => join(__dirname, str)
+const create = tasks => new Fly({tasks, plugins})
+
+test('fly-<%= pluginSlugName %>', t => {
+  t.plan(2)
+  const fly = create({
+    *foo(f) {
+      t.true('<%= pluginSlugName %>' in f, 'attach `<%= pluginSlugName %>` to Task instance')
+      t.true('<%= pluginSlugName %>' in fly.plugins, 'attach `<%= pluginSlugName %>` plugin to instance')
+    }
+  })
+  fly.start('foo')
 })
